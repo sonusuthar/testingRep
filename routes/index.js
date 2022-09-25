99609e46acded23b42dc031afb855b406635769d
sonu suthar <sonusuthar432@gmail.com>
2022-09-25 17:28:13 +0530

    add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+const connection = mysql.createConnection(
+  {
+    host: 'ec2-35-78-173-52.ap-northeast-1.compute.amazonaws.com',
+    port: 3306,
+    database: 'database-test',
+    user: 'admin',
+    password: 'Octal#321'
+  });
+let a;
+  connection.connect(function (err) {
+    if(err){
+       a="error occurred while connecting";
+        console.log("error occurred while connecting");
+    }
+    else{
+      a="connection created with Mysql successfully";
+        console.log("connection created with Mysql successfully");
+    }
+ });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title:a });
 });
 
 module.exports = router;
